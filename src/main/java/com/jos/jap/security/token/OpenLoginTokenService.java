b9b27976f5357a02b56252d3efea84e1196e6400
Amos Li <amos.li@hkbn.com.hk>
2021-02-08 17:20:19 +0800

    Security

--- src/main/java/com/jos/jap/security/token/OpenLoginTokenService.java
+++ src/main/java/com/jos/jap/security/token/OpenLoginTokenService.java
@@ -2,7 +2,11 @@ package com.jos.jap.security.token;
 
 import com.jos.jap.core.constant.ChannelEnum;
 import com.jos.jap.core.constant.SocialConstant;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,7 +22,7 @@ import org.springframework.social.security.SocialAuthenticationServiceLocator;
 import org.springframework.social.security.SocialAuthenticationToken;
 import org.springframework.social.security.provider.SocialAuthenticationService;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
+import org.springframework.util.Assert;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Comparator;
@@ -26,11 +30,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class OpenLoginTokenService {
+public class OpenLoginTokenService implements ApplicationContextAware {
 
     private TokenGranter tokenGranter;
 
     private OAuth2RequestFactory oAuth2RequestFactory;
+
+    private SocialAuthenticationServiceLocator authServiceLocator;
+    private ApplicationContext applicationContext;
 
     public OpenLoginTokenService(TokenGranter tokenGranter,
                                  OAuth2RequestFactory oAuth2RequestFactory) {
@@ -41,8 +48,8 @@ public class OpenLoginTokenService {
     private AuthorizationServerEndpointsConfigurer endpoints = new AuthorizationServerEndpointsConfigurer();
 
     public String loginForToken(HttpServletRequest request) {
-//        // 封装请求参数
-//        Authentication authRequest = attemptAuthentication(request);
+        // 封装请求参数
+        Authentication authRequest = attemptAuthentication(request);
         // 认证
 //        Authentication authentication = authenticationProvider.authenticate(authRequest);
 //        // 设置登录成功
@@ -52,24 +59,30 @@ public class OpenLoginTokenService {
         return token.toString();
     }
 
-//    protected Authentication attemptAuthentication(HttpServletRequest request) {
-//        String providerId = request.getParameter(SocialConstant.PARAM_PROVIDER);
-//        String channel = StringUtils.defaultIfBlank(request.getParameter(SocialConstant.PARAM_CHANNEL), ChannelEnum.pc.name());
-//        String providerUserId = request.getParameter(SocialConstant.PARAM_OPEN_ID);
-//        String openAccessToken = request.getParameter(SocialConstant.PARAM_OPEN_ACCESS_TOKEN); // 用于验证三方用户已授权
-//
-//        String uniqueProviderId = providerId + "@" + channel;
-//
-//        ConnectionData connectionData = new ConnectionData(uniqueProviderId, providerUserId, null, null, null, openAccessToken, null, null, null);
-//        SocialAuthenticationService<?> authService = getAuthServiceLocator().getAuthenticationService("1234");
-//        Connection connection = authService.getConnectionFactory().createConnection(connectionData);
-//        connection.sync();
-//        return new SocialAuthenticationToken(connection, null);
-//    }
-//
-//    public SocialAuthenticationServiceLocator getAuthServiceLocator() {
-//        return this.authServiceLocator;
-//    }
+    protected Authentication attemptAuthentication(HttpServletRequest request) {
+        String providerId = request.getParameter(SocialConstant.PARAM_PROVIDER);
+        String channel = StringUtils.defaultIfBlank(request.getParameter(SocialConstant.PARAM_CHANNEL), ChannelEnum.pc.name());
+        String providerUserId = request.getParameter(SocialConstant.PARAM_OPEN_ID);
+        String openAccessToken = request.getParameter(SocialConstant.PARAM_OPEN_ACCESS_TOKEN); // 用于验证三方用户已授权
+
+        String uniqueProviderId = providerId + "@" + channel;
+
+        ConnectionData connectionData = new ConnectionData(uniqueProviderId, providerUserId, null, null, null, openAccessToken, null, null, null);
+        SocialAuthenticationService<?> authService = getAuthServiceLocator().getAuthenticationService("1234");
+        Connection connection = authService.getConnectionFactory().createConnection(connectionData);
+        connection.sync();
+        return new SocialAuthenticationToken(connection, null);
+    }
+
+
+    public SocialAuthenticationServiceLocator getAuthServiceLocator() {
+        if (this.authServiceLocator == null) {
+            this.authServiceLocator = applicationContext.getBean(SocialAuthenticationServiceLocator.class);
+            Assert.notNull(this.authServiceLocator, "OpenLoginTokenService depends on SocialAuthenticationServiceLocator. " +
+                    "No single bean of that type found in application context.");
+        }
+        return this.authServiceLocator;
+    }
 
     protected OAuth2AccessToken createAccessToken() {
 
@@ -92,5 +105,10 @@ public class OpenLoginTokenService {
 
     protected TokenGranter getTokenGranter() {
         return tokenGranter;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
